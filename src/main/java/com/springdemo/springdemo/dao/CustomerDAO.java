352caff83b537a274af3c9212a8b321b508e3e27
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2022-10-12 12:30:47 +0600

    spring database rest -init

---
+++ src/main/java/com/springdemo/springdemo/dao/CustomerDAO.java
@@ -0,0 +1,17 @@
+package com.springdemo.springdemo.dao;
+
+import java.util.List;
+
+import com.springdemo.springdemo.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
+}
